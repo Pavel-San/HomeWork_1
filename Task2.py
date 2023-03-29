76972448883e5cd64f49259a3402b7753b118154
Pavel Sanin <sanin8383@gmail.com>
2023-03-29 23:07:43 +0300

    HW3

--- Task2.py
+++
@@ -1,4 +0,0 @@
-time_sec = int(input('Введите время в секундах:'))
-hours = time_sec // 3600
-minutes = time_sec // 60
-print('Время в формате ч:м:с -', hours, ':', minutes, ':', time_sec)
--- Task3.py
+++
@@ -1,3 +0,0 @@
-var = input('Введите число n:')
-var = str(int(var) + int(var + var) + int(var + var + var))
-print('n + nn + nnn =', var)
